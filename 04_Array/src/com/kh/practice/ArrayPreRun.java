492ae2c0173db817d9aee1148565605538a53df1
Kyungnan Kim <kht241022@gmail.com>
2024-11-20 12:22:39 +0900

    241120 조건문 활용해서 반복하여 원하는 기능 출력하기

--- 04_Array/src/com/kh/practice/ArrayPreRun.java
+++ 04_Array/src/com/kh/practice/ArrayPreRun.java
@@ -6,8 +6,9 @@ public class ArrayPreRun {
         ArrayPre1  arrayPre1 = new ArrayPre1();
         //arrayPre1.practice3();
         //arrayPre1.practice4();
-        //.practice6();
-        arrayPre1.practice6A();
+        //arrayPre1.practice6();
+        //arrayPre1.practice6A();
+        arrayPre1.practice6B();
     }
 
 }
