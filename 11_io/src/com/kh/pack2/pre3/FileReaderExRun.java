73affc49541b8147f4374dc9e204bf5100eedbe4
Kyungnan Kim <kht241022@gmail.com>
2024-11-29 17:17:44 +0900

    11-29 File FileWriter FileReader BufferedReader

---
+++ 11_io/src/com/kh/pack2/pre3/FileReaderExRun.java
@@ -0,0 +1,12 @@
+package com.kh.pack2.pre3;
+
+public class FileReaderExRun {
+
+    public static void main(String[] args) {
+        //FileReaderEx 내 method1() 기능을 사용해서 텍스트 파일을 읽고 출력
+        // lunch.txt 읽고 출력
+        FileReaderEx ex = new FileReaderEx();
+        ex.method1();
+    }
+
+}
