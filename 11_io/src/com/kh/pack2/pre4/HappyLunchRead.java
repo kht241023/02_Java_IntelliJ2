3aac59eaf2e4c8b22b2ed99821295c8a63c7af97
Kyungnan Kim <kht241022@gmail.com>
2024-12-02 09:50:10 +0900

    12-02 File FileWriter FileReader BufferedReader

--- 11_io/src/com/kh/pack2/pre4/HappyLunchRead.java
+++ 11_io/src/com/kh/pack2/pre4/HappyLunchRead.java
@@ -1,19 +1,12 @@
-package com.kh.pack2.pre3;
+package com.kh.pack2.pre4;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class FileReaderPre {
-    // 파일을 읽고 읽은 내용을 눈으로 확인
-    // happyLunch.txt
-    /**
-     * readTxt(String path, String fileName)
-     *
-     * @param path     = 파일경로
-     * @param fileName = 파일이름
-     * */
+public class HappyLunchRead {
+
     public void readTxt(String path, String fileName){
         try {
             FileReader fr = new FileReader(path + fileName);
--- 11_io/src/com/kh/pack2/pre4/HappyLunchReadRun.java
+++ 11_io/src/com/kh/pack2/pre4/HappyLunchReadRun.java
@@ -1,13 +1,13 @@
-package com.kh.pack2.pre3;
+package com.kh.pack2.pre4;
 
-import com.kh.pack2.pre4.HappyLunchRead;
+import com.kh.pack2.pre3.FileReaderPre;
 
-public class FileReaderPreRun {
+public class HappyLunchReadRun {
     public static void main(String[] args) {
         String path = System.getProperty("user.home") + "/Desktop/";
         String fileName = "happyLunch.txt";
 
-        FileReaderPre frp = new FileReaderPre();
+        HappyLunchRead frp = new HappyLunchRead();
 
         System.out.println("[" + fileName + "] 읽기 모드를 시작합니다.");
         frp.readTxt(path, fileName);
